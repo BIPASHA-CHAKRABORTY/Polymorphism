5d1ac23a6144a36f71079693d76a0c58c4be457d
Bipasha Chakraborty <86914435+BIPASHA-CHAKRABORTY@users.noreply.github.com>
2024-08-09 00:41:52 +0530

    Create quadractic eq.cpp

---
+++ quadractic eq.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Quadratic 
+{
+    private:
+        int a, b, c,d;
+
+    public:
+    	
+    	Quadratic() : a(0), b(0), c(0),d(0) {}
+        void input() 
+		{
+            cout << "Enter value of a: ";
+            cin >> a;
+            cout << "Enter value of b: ";
+            cin >> b;
+            cout << "Enter value of c: ";
+            cin >> c;
+        }
+    
+
+        void discriminant() 
+		{
+            d = b * b - 4 * a * c;
+            if (d == 0)
+                cout << "The quadratic equation doesn't have distinct roots." << endl;
+            else if (d > 0)
+                cout << "The quadratic equation has real roots." << endl;
+            else 
+                cout << "The quadratic equation has imaginary roots." << endl;
+        }
+
+        void display() 
+		{
+            cout << "Values of a, b, and c are: " << a << ", " << b << ", " << c << endl;
+            cout << "Discriminant value is: " <<d<< endl;
+        }
+};
+
+int main() 
+{
+    Quadratic Q;
+    Q.input();
+    Q.discriminant();
+    Q.display();
+
+    return 0;
+}
